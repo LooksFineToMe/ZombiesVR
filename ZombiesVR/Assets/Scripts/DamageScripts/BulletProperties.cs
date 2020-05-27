2d7993a3cd955e6e9d1c3248fba81c923ca9046b
Matthew <Jarrodx17@hotmail.com>
2020-05-27 11:24:14 +1000

    Updated Gun Blockout

---
+++ ZombiesVR/Assets/Scripts/DamageScripts/BulletProperties.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletProperties : MonoBehaviour
+{
+    public float timeToDestroy;
+    void Start()
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")) GetComponent<AIZombie>();
+    }
+}
