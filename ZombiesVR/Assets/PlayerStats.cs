bd8cd21d30dece570362180ee987d806d532546a
Matthew <Jarrodx17@hotmail.com>
2020-05-27 10:05:13 +1000

    Added Temp Animation, player health script

---
+++ ZombiesVR/Assets/PlayerStats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Header("Player's Stats")]
+    public int health = 5;
+    public int magCount;
+
+    public void TakeDamage()
+    {
+        health -= 1;
+        if (health <= 0)
+        {
+            GameOver();
+        }
+    }
+    private void GameOver()
+    {
+        print("Game is Over");
+    }
+}
