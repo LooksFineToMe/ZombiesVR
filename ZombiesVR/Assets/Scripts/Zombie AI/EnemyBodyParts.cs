6606efc03c0f7ca698ca05bfc6f6826509f9d980
Matthew <Jarrodx17@hotmail.com>
2020-05-28 14:42:22 +1000

    Created body part damage

---
+++ ZombiesVR/Assets/Scripts/Zombie AI/EnemyBodyParts.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBodyParts : MonoBehaviour
+{
+    public AIZombie aiZombie;
+    public int extraDamage;
+
+    public void DamageBodyPart(int damagesource)
+    {
+        aiZombie.TakePlayerDamage(damagesource + extraDamage);
+    }
+}
