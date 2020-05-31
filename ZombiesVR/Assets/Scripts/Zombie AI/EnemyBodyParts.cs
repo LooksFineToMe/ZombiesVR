efe88763d55a4333a45fc6853e7ae12f012cd895
Matthew <Jarrodx17@hotmail.com>
2020-05-31 19:00:39 +1000

    Added Zombie hit animation, added detachable body parts logic

--- ZombiesVR/Assets/Scripts/Zombie AI/EnemyBodyParts.cs
+++ ZombiesVR/Assets/Scripts/Zombie AI/EnemyBodyParts.cs
@@ -6,10 +6,20 @@ public class EnemyBodyParts : MonoBehaviour
 {
     public AIZombie aiZombie;
     public int extraDamage;
+    public bool isDetachable;
+    public Vector3 detachedScale;
     
     //added "knocked" bool to AI.TAKEPLAYERDAMAGE
     public void DamageBodyPart(int damagesource/*, bool knocked*/)
     {
         aiZombie.TakePlayerDamage(damagesource + extraDamage/*, knocked*/);
+        if (isDetachable == true)
+        {
+            DetachGameObject();
+        }
+    }
+    void DetachGameObject()
+    {
+        gameObject.transform.localScale = detachedScale;
     }
 }
