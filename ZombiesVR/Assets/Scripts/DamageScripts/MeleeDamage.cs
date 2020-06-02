c7e7f1ec2f6cdcc6740a1dd4fa123edc7bf148fc
Jay <jaydeo4@hotmail.com>
2020-06-02 15:09:17 +1000

    hope for the best

--- ZombiesVR/Assets/Scripts/DamageScripts/MeleeDamage.cs
+++ ZombiesVR/Assets/Scripts/DamageScripts/MeleeDamage.cs
@@ -32,16 +32,16 @@ public class MeleeDamage : MonoBehaviour
             
             Quaternion rot = Quaternion.FromToRotation(Vector3.forward, contactPoint.normal);
 
-            if (collision.rigidbody != null && collision.rigidbody.GetComponentInParent<RagdollHelper>() != null)
+            if (collision.rigidbody != null && collision.rigidbody.GetComponentInParent<AIZombie>() != null)
             {
                 
                 //find the RagdollHelper component and activate ragdolling
 
-                RagdollHelper helper = collision.rigidbody.GetComponentInParent<RagdollHelper>();
+                AIZombie zombie = collision.rigidbody.GetComponentInParent<AIZombie>();
                 //print(hit.collider.GetComponentInParent<RagdollHelper>().ToString());
                 
-                helper.anim = collision.rigidbody.GetComponentInParent<Animator>();
-                //we need to find a way to set the animator of the object being hit
+                //zombie.anim = collision.rigidbody.GetComponentInParent<Animator>();
+                ////we need to find a way to set the animator of the object being hit
 
 
                 //set the impact target to whatever the ray hit
@@ -53,7 +53,7 @@ public class MeleeDamage : MonoBehaviour
                 //might stretch
                 if (collision.rigidbody.GetComponentInParent<RagdollHelper>().ragdolled == false && rb.velocity.magnitude >= minimumVelocity)
                 {
-                    helper.ragdolled = true;
+                    StartCoroutine(zombie.ZombieRagdoll());
                     collision.rigidbody.GetComponent<EnemyBodyParts>().DamageBodyPart(axeDamage);
                     //impactTarget.AddForce(axePos.transform.forward * (axeForce * rb.velocity.magnitude), ForceMode.VelocityChange);
                     impactTarget.AddForce(-contactPoint.normal * (axeForce * rb.velocity.magnitude), ForceMode.VelocityChange);
@@ -62,7 +62,7 @@ public class MeleeDamage : MonoBehaviour
                 else if (rb.velocity.magnitude > 1f && rb.velocity.magnitude < minimumVelocity && collision.gameObject.CompareTag("Enemy") && timer >= .2f && isBluntWeapon == true)
                 {
                     timer = 0;
-                    helper.ragdolled = true;
+                    StartCoroutine(zombie.ZombieRagdoll());
                     impactTarget.AddForce(-contactPoint.normal * (axeForce * rb.velocity.magnitude), ForceMode.VelocityChange);
                     collision.rigidbody.GetComponent<EnemyBodyParts>().BluntDamage(axeDamage);
                 }
