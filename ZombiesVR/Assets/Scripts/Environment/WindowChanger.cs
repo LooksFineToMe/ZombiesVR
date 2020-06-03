f86f05d76a634eb89a82ed9ea80ffc8dc34c2b2f
Matthew <Jarrodx17@hotmail.com>
2020-06-04 00:56:17 +1000

    Made a quick window script

---
+++ ZombiesVR/Assets/Scripts/Environment/WindowChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowChanger : MonoBehaviour
+{
+    public Material[] materials;
+    Material material;
+    float timer;
+    int ChangeWindowTime;
+    private void Start()
+    {
+        material = GetComponent<Material>();
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= ChangeWindowTime)
+        {
+            ChangeMaterial();
+        }
+    }
+    public void ChangeMaterial()    
+    {
+        int randomNumber = Random.Range(0, materials.Length);
+        material = materials[randomNumber];
+        ChangeWindowTime = Random.Range(0, 60);
+    }
+
+}
