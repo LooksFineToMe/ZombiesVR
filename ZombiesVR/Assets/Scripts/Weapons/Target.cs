92ab08ebd2e909fad96d91e543c520467e97925e
Jay <jaydeo4@hotmail.com>
2020-06-23 16:28:03 +1000

    added main menu music theme, updated some audio on targets

--- ZombiesVR/Assets/Scripts/Weapons/Target.cs
+++ ZombiesVR/Assets/Scripts/Weapons/Target.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Animator m_Animations;
+    private AudioSource m_AudioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Animations = GetComponent<Animator>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -17,6 +19,14 @@ public class Target : MonoBehaviour
         if (collision.collider.gameObject.CompareTag("projectile"))
         {
             m_Animations.SetTrigger("Hit");
+            m_AudioSource.PlayOneShot(m_AudioSource.clip);
         }
+    }
+
+    [ContextMenu("Hit")]
+    private void Function()
+    {
+        m_Animations.SetTrigger("Hit");
+        m_AudioSource.PlayOneShot(m_AudioSource.clip);
     }
 }
