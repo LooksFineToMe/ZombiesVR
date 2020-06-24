24d72e5b9c26bb4a3c427b7a03567bf1b665a586
Jay <jaydeo4@hotmail.com>
2020-06-24 11:03:01 +1000

    minor adjustments

---
+++ ZombiesVR/Assets/Scripts/Utility/FadeAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAudio : MonoBehaviour
+{
+    [SerializeField] float fadeTime = 1;
+    [SerializeField] float volume = 0.4f;
+    [SerializeField] AudioClip m_ClipToPlay;
+
+    private DoubleAudioSource d_Source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        d_Source = GetComponent<DoubleAudioSource>();
+        d_Source.CrossFade(m_ClipToPlay, volume, fadeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
